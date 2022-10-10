01e91187b36db37928dcd4220342e22bdaa50c22
Harshad Sharma <harshad@sharma.io>
2022-10-10 19:54:07 +0530

    Add settings and improve how blocks are inserted.

--- index.ts
+++ index.ts
@@ -1,11 +1,42 @@
 import '@logseq/libs'
+import { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin';
 import ElizaNode from 'elizanode'
 
+const settingsSchema: SettingSchemaDesc[] = [
+  {
+    key: "elizaShortcut",
+    type: "string",
+    default: "ctrl+shift+enter",
+    title: "Shortcut",
+    description: "Keyboard shortcut to call Eliza",
+  },
+]
+
+async function settings_are_valid() {
+  const shortcut = logseq.settings!["elizaShortcut"]
+  if (!shortcut) {
+    console.error("Keyboard shortcut not configured for Eliza.")
+    logseq.UI.showMsg(
+      "Please configure a keyboard shortcut for Eliza.",
+      "error"
+    )
+    return false
+  }
+  return true
+}
+
 async function main() {
+  logseq.useSettingsSchema(settingsSchema)
+  if (!await settings_are_valid()) {
+    console.error("Eliza settings are invalid, exiting.")
+    return
+  }
+
   var eliza = new ElizaNode()
+  const shortcut = logseq.settings!["elizaShortcut"]
 
   logseq.App.registerCommandShortcut({
-    binding: "ctrl+shift+enter",
+    binding: shortcut,
     mode: "global"
   },
     async () => {
@@ -14,9 +45,8 @@ async function main() {
 
       if (input) {
         const output = eliza.transform(input)
-        const formatted_output = `Eliza: *${output}*`
-        const reply_block = await logseq.Editor.insertBlock(current_block!.uuid, formatted_output, { sibling: true })
-        await logseq.Editor.insertBlock(reply_block!.uuid, "", { sibling: true })
+        await logseq.Editor.insertBlock(current_block!.uuid, output, { sibling: false })
+        await logseq.Editor.insertBlock(current_block!.uuid, "", { sibling: true })
       }
       else {
         console.log("Eliza: No input, skipping...")
